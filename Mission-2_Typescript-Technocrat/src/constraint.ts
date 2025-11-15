1b53ceabb400ac8e93e6634d40dd242ae0bfcbb9
Abu Naim <abunaim5@users.noreply.github.com>
2025-11-15 19:48:22 +0600

    complete module 6 video 6 practice (constraint in typescript)

---
+++ Mission-2_Typescript-Technocrat/src/constraint.ts
@@ -0,0 +1,36 @@
+//* We can use constraint to strictly define the types. Which property or value must be needed.
+
+type Student = {id: number, name: string, dateOfBirth: string}
+
+const addStudentToCourse = <T extends Student>(courseInfo: T) => {
+    return {
+        course: 'Next level web dev',
+        ...courseInfo
+    }
+};
+
+const student1 = {
+    id: 123,
+    name: 'Elon',
+    hasPen: true,
+    dateOfBirth: '2-2-2022'
+};
+
+const student2 = {
+    id: 234,
+    name: 'Zucker',
+    hasWatch: true,
+    dateOfBirth: '2-2-2022'
+};
+
+const student3 = {
+    id: 456,
+    name: 'Bill',
+    hasPen: false,
+    hasCar: true,
+    dateOfBirth: '2-2-2022'
+};
+
+const std1 = addStudentToCourse(student1);
+const std2 = addStudentToCourse(student2);
+const std3 = addStudentToCourse(student3);
